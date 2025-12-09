a3e8eba3ee09fb30c7e1fdda15741222f6f10376
이준서 <tmdqksdl2000@naver.com>
2025-12-09 11:02:57 +0900

    숙제 전 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import bg from './img/bg.png';
+import data from './data.js';
+
 
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -26,8 +31,8 @@ function App() {
         <div className='row'>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
             <button className='btn btn-primary'>주문하기</button>
           </div>
           <div className='col-md-4'>
